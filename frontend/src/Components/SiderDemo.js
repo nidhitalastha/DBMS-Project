6bd29f75aa2db613d329258ce2aaba2d4861b6a4
Nidhi Talastha <61732295+nidhitalastha@users.noreply.github.com>
2020-08-17 16:31:25 +0530

    updated code

--- frontend/src/Components/SiderDemo.js
+++ frontend/src/Components/SiderDemo.js
@@ -99,7 +99,7 @@ class sider extends Component {
           <Header
             style={{ background: "#fff", padding: 0, textAlign: "center" }}
           >
-            VISITOR AND STAFF MANAGEMENT
+            VISITOR AND STAFF MANAGEMENT SYSTEM
           </Header>
           <Content style={{ margin: "24px 16px 0" }}>
             <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
